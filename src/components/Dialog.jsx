46a9b59bf723f20a2b9ac90e7ee114561cdee8b9
mooncake <59047525+ferhatAksoy@users.noreply.github.com>
2022-07-05 16:23:19 +0300

    first commit

---
+++ src/components/Dialog.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+import Terms from './Terms'
+
+class Dialog extends React.Component {
+    render( ) {
+        return <Terms />
+    }
+}
+export default Dialog
